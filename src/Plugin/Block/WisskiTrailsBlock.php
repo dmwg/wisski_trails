a10f22a5510647abaac714a6adae7b6fbc6524b1
Oliver Baumann <oliver.baumann@uni-bayreuth.de>
2025-10-07 16:06:09 +0200

    Add a block plugin
    The block takes care of rendering the iframe template, and receives the
    required parameters from the attached service.

---
+++ src/Plugin/Block/WisskiTrailsBlock.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Drupal\wisski_trails\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\wisski_trails\Service\WisskiTrailsService;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a WissKI Trails block.
+ *
+ * @Block(
+ *   id = "wisski_trails_block",
+ *   admin_label = @Translation("WissKI Trails"),
+ *   category = @Translation("WissKI")
+ * )
+ *
+ * @phpstan-consistent-constructor
+ */
+class WisskiTrailsBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The WissKI Trails service.
+   *
+   * @var \Drupal\wisski_trails\Service\WisskiTrailsService
+   */
+  protected $wisskiTrailsService;
+
+  /**
+   * The logger.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * Constructs a new WisskiTrailsBlock instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration.
+   * @param string $plugin_id
+   *   The plugin ID.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\wisski_trails\Service\WisskiTrailsService $wisski_trails_service
+   *   The WissKI Trails service.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The logger.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, WisskiTrailsService $wisski_trails_service, LoggerInterface $logger) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->wisskiTrailsService = $wisski_trails_service;
+    $this->logger = $logger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    /** @var \Drupal\wisski_trails\Service\WisskiTrailsService $service */
+    $service = $container->get('wisski_trails.service');
+    /** @var \Psr\Log\LoggerInterface $logger */
+    $logger = $container->get('logger.channel.wisski_trails');
+    
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $service,
+      $logger
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $entity_id = $this->wisskiTrailsService->getCurrentEntityId();
+
+    if (!$entity_id) {
+      $this->logger->warning('No entity ID found for WissKI Trails block.');
+      return [];
+    }
+
+    $iframe_url = $this->wisskiTrailsService->buildIframeUrl($entity_id);
+
+    if (!$iframe_url) {
+      $this->logger->error('Could not build iframe URL for entity @id.', ['@id' => $entity_id]);
+      return [];
+    }
+
+    return [
+      '#theme' => 'wisski_trails_iframe',
+      '#iframe_url' => $iframe_url,
+      '#entity_id' => $entity_id,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge() {
+    return 0;
+  }
+
+}
---
+++ src/Service/WisskiTrailsService.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Drupal\wisski_trails\Service;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Service for WissKI Trails functionality.
+ */
+class WisskiTrailsService {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * The logger.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * Constructs a WisskiTrailsService object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The logger.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, RouteMatchInterface $route_match, LoggerInterface $logger) {
+    $this->configFactory = $config_factory;
+    $this->routeMatch = $route_match;
+    $this->logger = $logger;
+  }
+
+  /**
+   * Gets the current entity ID from the route.
+   *
+   * @return string|null
+   *   The entity ID or NULL if not found.
+   */
+  public function getCurrentEntityId() {
+    $entity = $this->getEntityFromRoute();
+    if ($entity) {
+        $this->logger->error("Entity ID:", $entity->id());
+      return $entity->id();
+    }
+
+    $this->logger->warning('No entity found in current route.');
+    return NULL;
+  }
+
+  /**
+   * Gets the entity from the current route.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|null
+   *   The entity or NULL if not found.
+   */
+  protected function getEntityFromRoute(): ?EntityInterface
+  {
+    $route_name = $this->routeMatch->getRouteName();
+    foreach ($this->routeMatch->getParameters() as $parameter) {
+      if ($parameter instanceof EntityInterface) {
+        return $parameter;
+      }
+    }
+    return NULL;
+  }
+
+  /**
+   * Builds the iframe URL.
+   *
+   * @param string $entity_id
+   *   The entity ID.
+   *
+   * @return string|null
+   *   The iframe URL or NULL if base_url is not configured.
+   */
+  public function buildIframeUrl($entity_id) {
+    $config = $this->configFactory->get('wisski_trails.settings');
+    $base_url = $config->get('base_url');
+
+    if (empty($base_url)) {
+      $this->logger->error('Base URL is not configured.');
+      return NULL;
+    }
+
+    return rtrim($base_url, '/') . '/' . $entity_id;
+  }
+
+  /**
+   * Checks if the block should be displayed.
+   *
+   * @return bool
+   *   TRUE if the block should be displayed, FALSE otherwise.
+   */
+  public function shouldDisplay() {
+    $entity_id = $this->getCurrentEntityId();
+    $config = $this->configFactory->get('wisski_trails.settings');
+    $base_url = $config->get('base_url');
+
+    return !empty($entity_id) && !empty($base_url);
+  }
+
+}
